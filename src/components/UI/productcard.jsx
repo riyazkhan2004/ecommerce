5176a4250f6af1023fd7752592107ff298e6dbee
riyazkhan2004 <riyazkhan03.official@gmail.com>
2023-02-13 20:16:41 +0530

    hello

--- src/components/UI/productcard.jsx
+++ src/components/UI/productcard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion } from "framer-motion";
+
 import { Col } from "reactstrap";
 import { Link } from "react-router-dom";
 const productimg = require("../../assets/products/coffee.png");
@@ -7,14 +7,12 @@ const productimg = require("../../assets/products/coffee.png");
 export function ProductCard() {
   return (
     <Col lg="3" md="2">
-      <motion.div
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
+      <div
         className="product__item"
       >
         <div className="product__image">
-          <motion.img
-            whileHover={{ scale: 0.9 }}
+          <img
+            
             src={productimg}
             alt="productimg"
           />
@@ -30,17 +28,16 @@ export function ProductCard() {
           <div className="price">120 Rs.</div>
         </div>
         <div className="product__card-bottom d-flex align-items-center justify-content-between">
-          <motion.span
+          <span
             style={{ scale: 2, marginLeft: "15vw", marginTop: -45,display: 'flex',
             alignItems: 'center',
             columnGap: '2.7rem',
             marginBottom: 0 }}
-            whileTap={{ scale: 1.3 }}
           >
             <i class="ri-add-circle-fill fa-fw"></i>
-          </motion.span>
+          </span>
         </div>
-      </motion.div>
+      </div>
     </Col>
   );
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,7 +3,6 @@ import { Helmet } from "../../components/Helmet/helmet";
 import { Container, Row, Col } from "reactstrap";
 import "./home.css";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
 import { Shop } from "../shop/shop";
 import { ProductList } from "../../components/UI/productlist";
 const logo = require("../../assets/products/cycle.jpeg");
@@ -24,22 +23,17 @@ export function Home() {
             </Col>
             <Col lg="6" md="6">
               <div className="hero_img">
-                <motion.img src={logo} alt="logo" initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }} />
+                <img src={logo} alt="logo"/>
               </div>
             </Col>
           </Row>
         </Container>
         <Link to="../shop">
-          <motion.button
-            whileTap={{ scale: 1.2 }}
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
+          <button
             className="shop__btn"
-            whileHover={{ scale: 1.2 }}
           >
             SHOP NOW
-          </motion.button>
+          </button>
         </Link>
       </section>
       <section className="trending__products">
